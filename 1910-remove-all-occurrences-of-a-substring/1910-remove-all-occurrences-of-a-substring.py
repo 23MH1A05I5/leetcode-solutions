4aaa6a6492fe8ad3c08cf5b75bbf0e71f7f01821
M NANDA KUMAR <23MH1A05I5@acoe.edu.in>
2026-09-16 15:26:58 +0530

    Time: 15 ms (24.71%), Space: 19.2 MB (62.86%) - LeetHub

---
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeOccurrences(self, s: str, part: str) -> str:
+        stack=[]
+        for ch in s:
+            stack.append(ch)
+            if len(stack)>=len(part):
+                if ''.join(stack[-len(part):])==part:
+                    for i in range(len(part)):
+                        stack.pop()
+        return ''.join(stack)
+
+        
