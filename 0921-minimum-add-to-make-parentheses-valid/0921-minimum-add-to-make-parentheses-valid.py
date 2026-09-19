d871f2d766d3a775a15ba8f82aa301f1b0598b47
M NANDA KUMAR <23MH1A05I5@acoe.edu.in>
2026-09-19 20:07:52 +0530

    Time: 0 ms (100%), Space: 19.3 MB (16.24%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+        stack=[]
+        count=0
+        for ch in s:
+            if ch=='(':
+                stack.append(ch)
+            else:
+                if stack:
+                    stack.pop()
+                else:
+                    count+=1
+        return count+len(stack)
